9bdb9b45099247fa154e5113f36732cb095cbbb6
ethanbergstrom <ethan.bergstrom@gmail.com>
2021-11-14 14:33:33 -0600

    Additional changes

--- build.ps1
+++ build.ps1
@@ -3,11 +3,16 @@ $imagename = "$username/duoauthproxy"
 
 $uri = [System.Uri]((Invoke-WebRequest -Uri https://duo.com/docs/checksums).links | Where-Object href -Like '*duoauthproxy*src*' | Select-Object -ExpandProperty href)
 $version = ([System.Version]($uri.LocalPath).Split('-')[1]).ToString()
-$versiontag = "${imagename}:$version"
-$latesttag = "${imagename}:latest"
 
-podman build . --build-arg URI=$uri -t $versiontag
+$latestContainerVersion = [System.Version](podman search $imagename --list-tags --format=json | ConvertFrom-Json | Select-Object -ExpandProperty Tags | Where-Object {$_ -ne 'latest'} | Sort-Object -Descending | Select-Object -First 1)
 
-podman login docker.io -u $username -p $env:DOCKER_KEY
-podman push $versiontag docker://docker.io/$versiontag
-podman push $versiontag docker://docker.io/$latesttag
+if ($version -lt $latestContainerVersion) {
+    $versiontag = "${imagename}:$version"
+    $latesttag = "${imagename}:latest"
+
+    podman build . --build-arg URI=$uri -t $versiontag
+
+    podman login docker.io -u $username -p $env:DOCKER_KEY
+    podman push $versiontag docker://docker.io/$versiontag
+    podman push $versiontag docker://docker.io/$latesttag
+}
